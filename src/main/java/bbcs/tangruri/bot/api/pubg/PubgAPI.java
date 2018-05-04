7c7b34c2b372f641e798e17eb0dd2b89eb9c08ab
Eric Y. Kang <suni1217g@gmail.com>
2018-05-05 06:24:41 +0900

    server status : recent option added

--- src/main/java/bbcs/tangruri/bot/api/pubg/PubgAPI.java
+++ src/main/java/bbcs/tangruri/bot/api/pubg/PubgAPI.java
@@ -15,6 +15,10 @@ public class PubgAPI implements API {
 		STAT, SERVER
 	}
 
+	private enum ServerParameter {
+		RECENT
+	}
+
 	public String callAPI(List<String> params) {
 		if(params.size() < 1)
 			return null;
@@ -34,16 +38,23 @@ public class PubgAPI implements API {
 				ret = stat(params.get(1));
 				break;
 			case SERVER:
-				ret = serverStat();
+				boolean recent = false;
+				if(params.size() == 2 && params.get(1).equalsIgnoreCase("recent"))
+					recent = true;
+				ret = serverStat(recent);
 				break;
 		}
 
 		return ret;
 	}
 
-	private String serverStat() {
+	private String serverStat(boolean recent) {
 		int gap = 10;	//10분간격
 		int totalCnt = 60;	//최근 60분간
+		if(recent) {
+			gap = 1;
+			totalCnt = 15;
+		}
 		Document doc = null;
 		try {
 			doc = Jsoup.connect("https://dak.gg/statistics/server").get();
@@ -53,14 +64,14 @@ public class PubgAPI implements API {
 
 		String line = System.getProperty("line.separator");
 		StringBuilder sb = new StringBuilder();
-		sb.append("최근 1시간 서버 상태 : " + line);
+		sb.append("최근 " + getTimeString(totalCnt) + "서버 상태 : " + line);
 		int cnt = 0;
 		Elements rows = doc.select("tr");
 		for(Element row : rows) {
 			Elements cols = row.select("div.min");
 			for (int i = cols.size() - 1; i >= 0; i--) {
 				cnt++;
-				if ((cnt % gap) != 1) {
+				if (!recent && (cnt % gap) != 1) {
 					continue;
 				}
 				sb.append("time: " + cols.get(i).attr("title").replace("<br />", " status :"));
@@ -73,6 +84,16 @@ public class PubgAPI implements API {
 		}
 
 		return sb.toString();
+	}
+
+	private String getTimeString(int min) {
+		if(min < 60)
+			return min + "분";
+
+		int hour = (min + 1) % 60;
+		int minT = min - (hour * 60);
+
+		return hour + "시간" + (minT == 0 ? "" : (minT + "분"));
 	}
 
 	private String stat(String id) {
