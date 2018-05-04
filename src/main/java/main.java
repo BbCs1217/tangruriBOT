9ee56745681e4202034f6e0779bd0838f6a8d653
Eric Y. Kang <suni1217g@gmail.com>
2018-05-05 00:46:11 +0900

    token file read

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -4,9 +4,29 @@ import de.btobastian.javacord.Javacord;
 import de.btobastian.javacord.entities.message.Message;
 import de.btobastian.javacord.listener.message.MessageCreateListener;
 
+import java.io.*;
+
 public class main {
 	public static void main(String args[]) {
-		DiscordAPI api = Javacord.getApi("NDQxNTE0NTU1NDAzNjY1NDI4.DcycOA.4w5uVsQzRr11Krlftse9T3XnMFg", true);
+		String token = null;
+		BufferedReader in = null;
+		try {
+			in = new BufferedReader(new FileReader(Thread.currentThread().getContextClassLoader().getResource("token").getFile()));
+			token = in.readLine();
+		} catch (FileNotFoundException e) {
+			System.out.println("token file not exist");
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			if(in != null) {
+				try {
+					in.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		DiscordAPI api = Javacord.getApi(token, true);
 		api.connect(new FutureCallback<DiscordAPI>() {
 			public void onSuccess(DiscordAPI discordAPI) {
 				discordAPI.registerListener(new MessageCreateListener() {
